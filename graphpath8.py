04c776bbb54cd791e3a9570e6d36ad5b6d803d3f
dan <danlove99@hotmail.co.uk>
2020-03-24 22:23:42 +0000

    made an actual maze(sort of)

---
+++ graphpath8.py
@@ -0,0 +1,111 @@
+import tkinter as tk
+#from nodes import *
+root = tk.Tk()
+canvas = tk.Canvas(root, width=800, height=800)
+canvas.pack()
+
+a1 = canvas.create_rectangle(0,0,100,100,fill='black')
+a2 = canvas.create_rectangle(100,0,200,100,fill='black')
+a3 = canvas.create_rectangle(200,0,300,100,fill='black')
+a4 = canvas.create_rectangle(300,0,400,100,fill='green')
+a5 = canvas.create_rectangle(400,0,500,100,fill='green')
+a6 = canvas.create_rectangle(500,0,600,100,fill='green')
+a7 = canvas.create_rectangle(600,0,700,100,fill='green')
+a8 = canvas.create_rectangle(700,0,800,100,fill='blue')
+
+
+b1 = canvas.create_rectangle(0,100,100,200,fill='green')
+b2 = canvas.create_rectangle(100,100,200,200,fill='black')
+b3 = canvas.create_rectangle(200,100,300,200,fill='black')
+b4 = canvas.create_rectangle(300,100,400,200,fill='green')
+b5 = canvas.create_rectangle(400,100,500,200,fill='black')
+b6 = canvas.create_rectangle(500,100,600,200,fill='black')
+b7 = canvas.create_rectangle(600,100,700,200,fill='black')
+b8 = canvas.create_rectangle(700,100,800,200,fill='black')
+
+
+c1 = canvas.create_rectangle(0,200,100,300,fill='green')
+c2 = canvas.create_rectangle(100,200,200,300,fill='green')
+c3 = canvas.create_rectangle(200,200,300,300,fill='green')
+c4 = canvas.create_rectangle(300,200,400,300,fill='green')
+c5 = canvas.create_rectangle(400,200,500,300,fill='green')
+c6 = canvas.create_rectangle(500,200,600,300,fill='green')
+c7 = canvas.create_rectangle(600,200,700,300,fill='black')
+c8 = canvas.create_rectangle(700,200,800,300,fill='black')
+
+
+d1 = canvas.create_rectangle(0,300,100,400,fill='black')
+d2 = canvas.create_rectangle(100,300,200,400,fill='black')
+d3 = canvas.create_rectangle(200,300,300,400,fill='green')
+d4 = canvas.create_rectangle(300,300,400,400,fill='black')
+d5 = canvas.create_rectangle(400,300,500,400,fill='black')
+d6 = canvas.create_rectangle(500,300,600,400,fill='green')
+d7 = canvas.create_rectangle(600,300,700,400,fill='black')
+d8 = canvas.create_rectangle(700,300,800,400,fill='black')
+
+
+e1 = canvas.create_rectangle(0,400,100,500,fill='black')
+e2 = canvas.create_rectangle(100,400,200,500,fill='green')
+e3 = canvas.create_rectangle(200,400,300,500,fill='green')
+e4 = canvas.create_rectangle(300,400,400,500,fill='black')
+e5 = canvas.create_rectangle(400,400,500,500,fill='black')
+e6 = canvas.create_rectangle(500,400,600,500,fill='green')
+e7 = canvas.create_rectangle(600,400,700,500,fill='black')
+e8 = canvas.create_rectangle(700,400,800,500,fill='black')
+
+
+f1 = canvas.create_rectangle(0,500,100,600,fill='black')
+f2 = canvas.create_rectangle(100,500,200,600,fill='black')
+f3 = canvas.create_rectangle(200,500,300,600,fill='black')
+f4 = canvas.create_rectangle(300,500,400,600,fill='black')
+f5 = canvas.create_rectangle(400,500,500,600,fill='black')
+f6 = canvas.create_rectangle(500,500,600,600,fill='green')
+f7 = canvas.create_rectangle(600,500,700,600,fill='black')
+f8 = canvas.create_rectangle(700,500,800,600,fill='black')
+
+
+g1 = canvas.create_rectangle(0,600,100,700,fill='black')
+g2 = canvas.create_rectangle(100,600,200,700,fill='green')
+g3 = canvas.create_rectangle(200,600,300,700,fill='green')
+g4 = canvas.create_rectangle(300,600,400,700,fill='green')
+g5 = canvas.create_rectangle(400,600,500,700,fill='green')
+g6 = canvas.create_rectangle(500,600,600,700,fill='green')
+g7 = canvas.create_rectangle(600,600,700,700,fill='green')
+g8 = canvas.create_rectangle(700,600,800,700,fill='green')
+
+
+h1 = canvas.create_rectangle(0,700,100,800,fill='black')
+h2 = canvas.create_rectangle(100,700,200,800,fill='blue')
+h3 = canvas.create_rectangle(200,700,300,800,fill='black')
+h4 = canvas.create_rectangle(300,700,400,800,fill='black')
+h5 = canvas.create_rectangle(400,700,500,800,fill='black')
+h6 = canvas.create_rectangle(500,700,600,800,fill='black')
+h7 = canvas.create_rectangle(600,700,700,800,fill='black')
+h8 = canvas.create_rectangle(700,700,800,800,fill='black')
+
+
+graph = {a8: [a7], a7:[a6],a6:[a5], a5:[a4], a4:[b4],
+        b4:[c4], c4:[c3, c5], c3:[c2,d3], c2:[c1],c1:[b1],
+        d3:[e3], e3:[e2], c5:[c6], c6:[d6], d6:[e6], e6:[f6], f6:[g6],
+        g6:[g5, g7], g5:[g4], g4:[g3],g3:[g2], g7:[g8], g2:[h2] }
+
+def find_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if not start in graph:
+        return None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath: return newpath
+    return None
+print(find_path(graph,a8,h2))
+path = find_path(graph, a8, h2)
+import time
+for i in path:
+    #time.sleep(1)
+    canvas.itemconfig(i, fill='red')
+
+
+root.mainloop()
---
+++ nodes.py
@@ -0,0 +1,115 @@
+from graphpath8 import *
+a1 = canvas.create_rectangle(0,0,100,100,fill='blue')
+a2 = canvas.create_rectangle(100,0,200,100,fill='green')
+a3 = canvas.create_rectangle(200,0,300,100,fill='green')
+a4 = canvas.create_rectangle(300,0,400,100,fill='green')
+a5 = canvas.create_rectangle(400,0,500,100,fill='green')
+a6 = canvas.create_rectangle(500,0,600,100,fill='green')
+a7 = canvas.create_rectangle(600,0,700,100,fill='green')
+a8 = canvas.create_rectangle(700,0,800,100,fill='green')
+
+
+b1 = canvas.create_rectangle(0,100,100,200,fill='green')
+b2 = canvas.create_rectangle(100,100,200,200,fill='green')
+b3 = canvas.create_rectangle(200,100,300,200,fill='green')
+b4 = canvas.create_rectangle(300,100,400,200,fill='green')
+b5 = canvas.create_rectangle(400,100,500,200,fill='green')
+b6 = canvas.create_rectangle(500,100,600,200,fill='green')
+b7 = canvas.create_rectangle(600,100,700,200,fill='green')
+b8 = canvas.create_rectangle(700,100,800,200,fill='green')
+
+
+c1 = canvas.create_rectangle(0,200,100,300,fill='green')
+c2 = canvas.create_rectangle(100,200,200,300,fill='green')
+c3 = canvas.create_rectangle(200,200,300,300,fill='green')
+c4 = canvas.create_rectangle(300,200,400,300,fill='green')
+c5 = canvas.create_rectangle(400,200,500,300,fill='green')
+c6 = canvas.create_rectangle(500,200,600,300,fill='green')
+c7 = canvas.create_rectangle(600,200,700,300,fill='green')
+c8 = canvas.create_rectangle(700,200,800,300,fill='green')
+
+
+d1 = canvas.create_rectangle(0,300,100,400,fill='green')
+d2 = canvas.create_rectangle(100,300,200,300,fill='green')
+d3 = canvas.create_rectangle(200,300,300,400,fill='green')
+d4 = canvas.create_rectangle(300,300,400,400,fill='green')
+d5 = canvas.create_rectangle(400,300,500,400,fill='green')
+d6 = canvas.create_rectangle(500,300,600,400,fill='green')
+d7 = canvas.create_rectangle(600,300,700,400,fill='green')
+d8 = canvas.create_rectangle(700,300,800,400,fill='green')
+
+
+e1 = canvas.create_rectangle(0,400,100,500,fill='green')
+e2 = canvas.create_rectangle(100,400,200,500,fill='green')
+e3 = canvas.create_rectangle(200,400,300,500,fill='green')
+e4 = canvas.create_rectangle(300,400,400,500,fill='green')
+e5 = canvas.create_rectangle(400,400,500,500,fill='green')
+e6 = canvas.create_rectangle(500,400,600,500,fill='green')
+e7 = canvas.create_rectangle(600,400,700,500,fill='green')
+e8 = canvas.create_rectangle(700,400,800,500,fill='green')
+
+
+f1 = canvas.create_rectangle(0,500,100,600,fill='green')
+f2 = canvas.create_rectangle(100,500,200,600,fill='green')
+f3 = canvas.create_rectangle(200,500,300,600,fill='green')
+f4 = canvas.create_rectangle(300,500,400,600,fill='green')
+f5 = canvas.create_rectangle(400,500,500,600,fill='green')
+f6 = canvas.create_rectangle(500,500,600,600,fill='green')
+f7 = canvas.create_rectangle(600,500,700,600,fill='green')
+f8 = canvas.create_rectangle(700,500,800,600,fill='green')
+
+
+g1 = canvas.create_rectangle(0,600,100,700,fill='green')
+g2 = canvas.create_rectangle(100,600,200,700,fill='green')
+g3 = canvas.create_rectangle(200,600,300,700,fill='green')
+g4 = canvas.create_rectangle(300,600,400,700,fill='green')
+g5 = canvas.create_rectangle(400,600,500,700,fill='green')
+g6 = canvas.create_rectangle(500,600,600,700,fill='green')
+g7 = canvas.create_rectangle(600,600,700,700,fill='green')
+g8 = canvas.create_rectangle(700,600,800,700,fill='green')
+
+
+h1 = canvas.create_rectangle(0,700,100,800,fill='green')
+h2 = canvas.create_rectangle(100,700,200,800,fill='green')
+h3 = canvas.create_rectangle(200,700,300,800,fill='green')
+h4 = canvas.create_rectangle(300,700,400,800,fill='green')
+h5 = canvas.create_rectangle(400,700,500,800,fill='green')
+h6 = canvas.create_rectangle(500,700,600,800,fill='green')
+h7 = canvas.create_rectangle(600,700,700,800,fill='green')
+h8 = canvas.create_rectangle(700,700,800,800,fill='green')
+
+'''
+X s o o X X X X 
+O X X o X X X X 
+O o o o o o X X 
+X X o X X o X X 
+X o o X X o X X 
+X X X X X o X X 
+X o o o o o o o 
+X f X X X X X X 
+
+Maze = 
+A2(A3)
+A3(A4)
+A4(b4)
+B4(C4)
+C4(c3, c5)
+C3(c2)
+C2(c1)
+C1(b1)
+B1()
+C5(c6)
+C6(d6)
+D6(e6)
+E6(f6)
+F6(g6)
+G6(g7,g5)
+G7(G8)
+G8()
+G5(g4)
+G4(G3)
+G3(G2)
+G2(G2)
+Start= A2
+Finish = G2
+'''
